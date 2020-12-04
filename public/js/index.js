2e8573fa1ebe291800bc766eb87862ca3a4949b2
Gabi <gari.327@gmail.com>
2020-12-04 14:00:43 +0100

    mas

--- public/js/index.js
+++ public/js/index.js
@@ -10,5 +10,29 @@ var ultimoParrafo = document.querySelector('section p:last-of-type');
 retocarButton.addEventListener('click', function (e) {
   ultimoParrafo.classList.add('retocar');
 });
+var fruitElem = document.querySelectorAll('ul li');
+var filtro = document.querySelector('#filtro');
+var salida = document.querySelector('#salida');
+filtro.addEventListener('keyup', function (e) {
+  var salidaValues = '';
+  fruitElem.forEach(function (fruit) {
+    if (fruit.textContent.includes(e.target.value)) salidaValues += fruit.textContent + ' | ';
+  });
+  console.log(salidaValues);
+  salida.textContent = salidaValues;
+});
+var footer = document.createElement('footer');
+footer.textContent = 'Copyright 2020';
+body.appendChild(footer);
+var padre = document.querySelector('#padre');
+var main = document.querySelector('main');
+padre.addEventListener('click', function () {
+  var button = document.createElement('button');
+  button.textContent = 'Hijo';
+  button.addEventListener('click', function (e) {
+    return padre.remove();
+  });
+  main.appendChild(button);
+});
 
 //# sourceMappingURL=index.js.map
--- src/app-color.js
+++ src/app-color.js
@@ -15,5 +15,29 @@ retocarButton.addEventListener('click', (e)=>{
 
 
 
+const fruitElem = document.querySelectorAll('ul li')
+const filtro = document.querySelector('#filtro')
+const salida = document.querySelector('#salida')
+filtro.addEventListener('keyup', (e) =>{
+	let salidaValues = ''
+	fruitElem.forEach(fruit => {
+		if(fruit.textContent.includes(e.target.value)) 	salidaValues+=fruit.textContent+' | '
+	})
+	console.log(salidaValues)
+	salida.textContent = salidaValues
+})
 
 
+const footer = document.createElement('footer')
+footer.textContent = 'Copyright 2020'
+body.appendChild(footer)
+
+
+const padre = document.querySelector('#padre')
+const main = document.querySelector('main')
+padre.addEventListener('click', () => {
+	const button = document.createElement('button')
+	button.textContent = 'Hijo'
+	button.addEventListener('click',(e) => padre.remove() )
+	main.appendChild(button)
+})
